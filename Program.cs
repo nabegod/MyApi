40e54625f95e39b0424a0f9fd55a9c760d1042dc
ana <anabeatrizsouzaalves94@gmail.com>
2025-06-23 09:06:53 -0300

    Minhas alterações locais

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona serviços de controladores
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+// Usa o roteamento de controladores
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
